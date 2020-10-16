7e340ff3693a6ed60f42474c925ea180bce76819
Duman Daulbekov <dumandaulbekov@gmail.com>
2020-10-17 02:05:19 +0500

    Add getById post

---
+++ api/getById.php
@@ -0,0 +1,19 @@
+<?php
+
+require "connect.php";
+
+$id = ($_GET['id'] !== null && (int)$_GET['id'] > 0)
+    ? mysqli_real_escape_string($con, (int)$_GET['id'])
+    : false;
+
+if (!$id) {
+    return http_response_code(400);
+}
+
+$sql = "SELECT * FROM `posts` WHERE id='{$id}'";
+
+if ($result = mysqli_query($con, $sql)) {
+    echo json_encode(mysqli_fetch_assoc($result));
+} else {
+    http_response_code(404);
+}
--- api/update.php
+++ api/update.php
@@ -4,7 +4,6 @@ require 'connect.php';
 
 $postdata = file_get_contents("php://input");
 
-
 if (isset($postdata) && !empty($postdata)) {
     $request = json_decode($postdata);
 
