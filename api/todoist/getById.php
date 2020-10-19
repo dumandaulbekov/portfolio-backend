b236b2523af096250c95b4b5f8be4592f76ca0b8
Duman Daulbekov <dumandaulbekov@gmail.com>
2020-10-19 17:03:24 +0500

    Add get by id todo

---
+++ api/todoist/getById.php
@@ -0,0 +1,19 @@
+<?php
+
+require "../connect.php";
+
+$id = ($_GET['id'] !== null && (int)$_GET['id'] > 0)
+    ? mysqli_real_escape_string($con, (int)$_GET['id'])
+    : false;
+
+if ($id) {
+    $sql = "SELECT * FROM `todoist` WHERE id='{$id}'";
+
+    if ($result = mysqli_query($con, $sql)) {
+        return json_encode(mysqli_fetch_assoc($result));
+    } else {
+        return http_response_code(404);
+    }
+} else {
+    return http_response_code(400);
+}
