c7442a0b784170e3ac1812389b999e7d929b732f
Duman Daulbekov <dumandaulbekov@gmail.com>
2020-10-19 17:04:56 +0500

    Update update-todo

--- api/post/update.php
+++ api/post/update.php
@@ -2,19 +2,19 @@
 
 require '../connect.php';
 
-$postdata = file_get_contents("php://input");
+$request = file_get_contents("php://input");
 
-if (isset($postdata) && !empty($postdata)) {
-    $request = json_decode($postdata);
+if (isset($request) && !empty($request)) {
+    $post = json_decode($request);
 
-    if ((int)$request->id < 1 || trim($request->title) == '' || trim($request->content) == '') {
+    if ((int)$post->id < 1 || trim($post->title) == '' || trim($post->content) == '') {
         return http_response_code(400);
     }
 
-    $id = mysqli_escape_string($con, (int)$request->id);
-    $title = mysqli_escape_string($con, trim($request->title));
-    $content = mysqli_escape_string($con, trim($request->content));
-    $modifiedDate = mysqli_real_escape_string($con, $request->modifiedDate);
+    $id = mysqli_escape_string($con, (int)$post->id);
+    $title = mysqli_escape_string($con, trim($post->title));
+    $content = mysqli_escape_string($con, trim($post->content));
+    $modifiedDate = mysqli_real_escape_string($con, $post->modifiedDate);
 
     $sql = "UPDATE `posts` SET `title`='$title', `content`='$content', `modifiedDate`='$modifiedDate' WHERE `id`='{$id}' LIMIT 1";
 
