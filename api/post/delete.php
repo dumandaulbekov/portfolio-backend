47275826e5c9bd8de2ce74c19d3de3b013219489
Duman Daulbekov <dumandaulbekov@gmail.com>
2020-10-19 17:05:19 +0500

    Update delete post

--- api/post/delete.php
+++ api/post/delete.php
@@ -6,14 +6,14 @@ $id = ($_GET['id'] !== null && (int)$_GET['id'] > 0)
     ? mysqli_real_escape_string($con, (int)$_GET['id'])
     : false;
 
-if (!$id) {
-    return http_response_code(400);
-}
+if ($id) {
+    $sql = "DELETE FROM `posts` WHERE `id` = '{$id}' LIMIT 1";
 
-$sql = "DELETE FROM `posts` WHERE `id` = '{$id}' LIMIT 1";
-
-if (mysqli_query($con, $sql)) {
-    http_response_code(204);
+    if (mysqli_query($con, $sql)) {
+        return http_response_code(204);
+    } else {
+        return http_response_code(422);
+    }
 } else {
-    return http_response_code(422);
+    return http_response_code(400);
 }
