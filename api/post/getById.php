6cc372dbb0d0087c6efeee7f6015ce5fa7348969
Duman Daulbekov <dumandaulbekov@gmail.com>
2020-10-19 17:05:10 +0500

    Update get by id post

--- api/post/getById.php
+++ api/post/getById.php
@@ -6,14 +6,14 @@ $id = ($_GET['id'] !== null && (int)$_GET['id'] > 0)
     ? mysqli_real_escape_string($con, (int)$_GET['id'])
     : false;
 
-if (!$id) {
-    return http_response_code(400);
-}
+if ($id) {
+    $sql = "SELECT * FROM `posts` WHERE id='{$id}'";
 
-$sql = "SELECT * FROM `posts` WHERE id='{$id}'";
-
-if ($result = mysqli_query($con, $sql)) {
-    echo json_encode(mysqli_fetch_assoc($result));
+    if ($result = mysqli_query($con, $sql)) {
+        echo json_encode(mysqli_fetch_assoc($result));
+    } else {
+        http_response_code(404);
+    }
 } else {
-    http_response_code(404);
+    return http_response_code(400);
 }
