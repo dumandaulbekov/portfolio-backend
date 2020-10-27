cfd894af7d430f13ed840e4cf623827882af3c6e
Duman Daulbekov <dumandaulbekov@gmail.com>
2020-10-27 18:33:34 +0500

    Refactor get all post

--- api/post/getAll.php
+++ api/post/getAll.php
@@ -1,32 +1,6 @@
 <?php
 
-require '../connect.php';
+require '../service/post.service.php';
 
-$getAll = new GetAllPost();
-$getAll->getAll();
-
-class GetAllPost {
-    public function getAll() {
-        $sql = "SELECT * FROM posts";
-        $con = connect();
-
-        if ($result = mysqli_query($con, $sql)) {
-            $posts = [];
-            $i = 0;
-
-            while ($row = mysqli_fetch_assoc($result)) {
-                $posts[$i]['id'] = $row['id'];
-                $posts[$i]['title'] = $row['title'];
-                $posts[$i]['content'] = $row['content'];
-                $posts[$i]['createdDate'] = $row['createdDate'];
-                $posts[$i]['modifiedDate'] = $row['modifiedDate'];
-
-                $i++;
-            }
-
-            echo json_encode($posts);
-        } else {
-            return http_response_code(404);
-        }
-    }
-}
+$postService = new PostService();
+echo $postService->getAll();
