43e88918ee21ffee19835bc34e3adba551607186
Duman Daulbekov <dumandaulbekov@gmail.com>
2020-10-27 18:33:54 +0500

    Refactor update post

--- api/post/update.php
+++ api/post/update.php
@@ -1,41 +1,24 @@
 <?php
-require "../connect.php";
 
-$update = new UpdatePost();
-$update->update();
+require '../service/post.service.php';
 
-class UpdatePost {
+$post = new Post();
+$post->update();
+
+class Post {
+    private $postService;
+
+    public function __construct() {
+        $this->postService = new PostService();
+    }
+
     public function update() {
-        $con = connect();
+        $post = json_decode(file_get_contents("php://input"));
 
-        $body = file_get_contents("php://input");
-
-        if (isset($body) && !empty($body)) {
-            $post = json_decode($body);
-
-            if ((int)$post->id < 1 || trim($post->title) == '' || trim($post->content) == '') {
-                return http_response_code(400);
-            }
-
-            $id = mysqli_escape_string($con, (int)$post->id);
-            $title = mysqli_escape_string($con, trim($post->title));
-            $content = mysqli_escape_string($con, trim($post->content));
-            $modifiedDate = mysqli_real_escape_string($con, $post->modifiedDate);
-
-            $sql = "UPDATE `posts` SET `title`='$title', `content`='$content', `modifiedDate`='$modifiedDate' WHERE `id`='{$id}' LIMIT 1";
-
-            if (mysqli_query($con, $sql)) {
-                $post = [
-                    'id' => $id,
-                    'title' => $title,
-                    'content' => $content,
-                    'modifiedDate' => $modifiedDate,
-                ];
-
-                echo json_encode($post);
-            } else {
-                return http_response_code(422);
-            }
+        if ((int)$post->id < 1 && trim($post->title === '' && $post->content === '')) {
+            return http_response_code(400);
         }
+
+        echo $this->postService->update($post->id, $post->title, $post->content);
     }
 }
